130b7951c0ff9873abfd4e0a414db207e00ec468
hoabui-dot <58062470+hoabui-dot@users.noreply.github.com>
2021-12-28 10:14:51 +0700

    update

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>Ultimate Guide  To</h1>
+            <h1>Planning Your Trip To</h1>
+        </div>
+    )
+}
+
+export default Home
